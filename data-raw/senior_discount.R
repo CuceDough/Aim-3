c89a91307d3b6c3ee5754a4476fe24949ff595fb
Carl Pearson <cap10@ufl.edu>
2015-05-16 12:00:47 -0400

    more data script updates

--- data-raw/senior_discount.R
+++ data-raw/senior_discount.R
@@ -2,37 +2,37 @@
 
 require(data.table)
 
-baseline <- c("0-4"=.38, "5-18"=.26, "19-29"=.09, "30-64"=.18, "65+"=.5) ## from cite what?
-baseline_iiv_rate <- .6 ## from cite what?
+senior_sp_reduction <- .70 ## from cite what?
 
-baseline_iiv <- c(rep(baseline_iiv_rate, 4), 1)*baseline
-baseline_laiv <- c(rep(1-baseline_iiv_rate, 4), 0)*baseline
+VE_hom <- c(
+  sp=.80,
+  s = .40,
+  p = .67
+) ## from cite what?
 
-background <- data.table(AgeGroup = names(baseline[-2]), LAIV=baseline_laiv[-2], IIV=baseline_iiv[-2])
+VE_het <- c(
+  sp=.40,
+  s = .30,
+  p = .14
+) ## from cite what?
 
-prop_LAIV <- 10/11 ## from cite what?
-
-SLIV_min <- .3
-SLIV_max <- .85
-SLIV_by <- 0.05
-
-SLIV_seq <- seq(SLIV_min, SLIV_max, SLIV_by)
-
-rate_laiv <- c(0, (SLIV_seq-baseline[2])*prop_LAIV) + baseline_laiv[2]
-rate_iiv <- c(0, (SLIV_seq-baseline[2])*(1-prop_LAIV)) + baseline_iiv[2]
-
-scenarios <- data.table(LAIV=rate_laiv, IIV=rate_iiv, total = rate_laiv + rate_iiv)
-
-seniorDiscount <- .70 ## from cite what?
-
-sen_VE_hom <- c(sp=.80, s = .40, p = .67)
-sen_VE_het <- c(sp=.40, s = .30, p = .14)
-
-alpha <- function(VE_sp = .80, VE_s = .40, VE_p = .67, s.d = seniorDiscount) {
-  qa <- -VE_s*VE_p; qb <- VE_s+VE_p; qc <- -s.d*VE_sp
-  (-qb+c(-1,1)*sqrt(qb^2 - 4*qa*qc))/(2*qa)
+s_and_p_reduction <- function(VE, s.d = senior_sp_reduction) {
+  ## assume reduction for VE_s, VE_p the same, solve quadratic eqn
+  a <- -VE["s"]*VE["p"]; b <- VE["s"]+VE["p"]; c <- -s.d*VE["sp"]
+  (-b + sqrt(b^2 - 4*a*c))/(2*a)
 }
 
-hom <- alpha()[2]*sen_VE_hom[-1]
-het <- alpha(.40, .30, .14)[2]*sen_VE_het[-1]
+hom <- s_and_p_reduction(VE_hom)*VE_hom[-1]
+het <- s_and_p_reduction(VE_het)*VE_het[-1]
 
+senior_VE = data.table(
+  scenario=factor(c('homologous','heterologous')),
+  VE_s = c(hom["s"],het["s"]),
+  VE_p = c(hom["p"],het["p"]),
+  key = "scenario"
+)
+
+require(devtools)
+
+devtools::use_data(senior_VE, overwrite = T)
+write.csv(senior_VE, file="./data-raw/senior_VE.csv", row.names = F)
--- data-raw/vax_scenario.R
+++ data-raw/vax_scenario.R
@@ -3,11 +3,11 @@ require(data.table)
 ## TODO require(arg.parse) or similar, use below as defaults but otherwise allow custom input
 
 baseline <- c(
-  "0-4"=.38,
-  "5-18"=.26,
-  "19-29"=.09,
-  "30-64"=.18,
-  "65+"=.5
+  "0-4" = 0.38,
+  "5-18" = 0.26,
+  "19-29" = 0.09,
+  "30-64" = 0.18,
+  "65+" = 0.50
 )
 ## from cite what?
 
@@ -16,7 +16,12 @@ baseline_tiv_rate <- .6 ## from cite what?
 baseline_tiv <- c(rep(baseline_tiv_rate, 4), 1)*baseline
 baseline_laiv <- c(rep(1-baseline_tiv_rate, 4), 0)*baseline
 
-background <- data.table(AgeGroup = names(baseline[-2]), LAIV=baseline_laiv[-2], TIV=baseline_tiv[-2])
+background <- data.table(
+  AgeGroup = factor(names(baseline[-2]), levels=names(baseline)),
+  LAIV=baseline_laiv[-2],
+  TIV=baseline_tiv[-2],
+  key="AgeGroup"
+)
 
 prop_LAIV <- 10/11 ## as SLIV rate increases, what proportion is LAIV vs TIV.
 ## from cite what?
@@ -30,6 +35,9 @@ SLIV_seq <- seq(SLIV_min, SLIV_max, SLIV_by)
 rate_laiv <- c(0, (SLIV_seq-baseline[2])*prop_LAIV) + baseline_laiv[2]
 rate_tiv <- c(0, (SLIV_seq-baseline[2])*(1-prop_LAIV)) + baseline_tiv[2]
 
-scenarios <- data.table(LAIV=rate_laiv, TIV=rate_tiv)
+scenarios <- data.table(scenario=0:length(SLIV_seq), LAIV=rate_laiv, TIV=rate_tiv,key = "scenario")
 
 stopifnot(all.equal(scenarios[,LAIV + TIV], c(baseline[2], SLIV_seq), tol=10^-10, check.names=F))
+
+require(devtools)
+devtools::use_data(background, scenarios, overwrite = T)
